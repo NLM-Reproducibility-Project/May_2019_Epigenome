11a334228cb2345a8971205b9d5e9cc44fcf5b0c
ggoodney <40798664+ggoodney@users.noreply.github.com>
2019-05-17 11:23:08 -0400

    Update pca_dmr.R

--- R/pca_dmr.R
+++ R/pca_dmr.R
@@ -13,6 +13,9 @@ sdat <- t(scale(t(matr[,5:40])))
 
 pr.dis <- dist(t(sdat), method = "euclidean")
 hclust(pr.dis,method = "complete") -> xx
+
+##this is best dendo viz
+fviz_dend(xx, rect = T, cex = 0.5, k = 10, k_colors = "rickandmorty")
 xx <-as.dendrogram(xx)
 ##colored by label
 xx %>% set("labels_colors", k = 10) %>% plot(main = "Default colors")
